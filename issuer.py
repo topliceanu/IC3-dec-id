80f6bb4eca77dbe1c56a452ab5ac750c931bf1ea
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2023-06-16 16:31:28 -0400

    rename web_api to issuer

--- issuer.py
+++ issuer.py
@@ -1,17 +1,22 @@
-import subprocess
+from flask import Flask
 
-command = [
-    "docker", 
-    "run", 
-    "-i", 
-    "--rm", 
-    "-e", 
-    "GOLOG_LOG_LEVEL=info", 
-    "--network=host", 
-    "deco:v0.8.1-rc1"
-]
+app = Flask(__name__)
 
-with open('issuer.json', 'r') as file:
-    # output = subprocess.run(command, stdin=file, text=True, capture_output=True)
-    output = subprocess.check_output(command, stdin=file, text=True)
-    print(output)
+@app.route("/post/issue")
+def issue():
+    data = request.json
+    attestation, commitment, proof = data.attestation, data.commitment, data.proof
+
+    # run attestation check
+    if not check_deco_attestation_sig(attestation):
+        return  '{"error": "deco attestation signature check failed"}', status.HTTP_400_BAD_REQUEST, { Content-Type: "application/json" }
+
+    # typecheck commitment
+
+# @app.route("/post/register")
+# def register_voter():
+#     pass
+#
+# @app.route("/post/vote")
+# def vote():
+#     pass
