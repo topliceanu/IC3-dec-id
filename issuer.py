6e9a164b5add3408dfe3dd309d8c113d5375b35e
Ulysses Kee <ulysseskcw96@gmail.com>
2023-06-17 16:34:45 -0400

    working workflow from init attestation to signed commitment

--- issuer.py
+++ issuer.py
@@ -1,9 +1,12 @@
 import json
+from generate_sig import generate_keys, mimc_signature
 from flask import Flask, request
 
 from attestation_check import is_attestation_valid
 
 app = Flask(__name__)
+
+issuer_sk, issuer_pk = generate_keys(True) # make random=True
 
 @app.route("/issue", methods = ["POST"])
 def issue():
@@ -16,7 +19,18 @@ def issue():
 
     # typecheck commitment
 
-    return "success", 200
+    # sign commitment
+    signed_commitment = mimc_signature(commitment, issuer_sk, issuer_pk)
+    print("signed commitment on the server", signed_commitment)
+
+    payload = {
+        "success": "true", 
+        "data": {
+            "commitment": commitment
+        }
+    }
+
+    return json.dumps(payload), 200, { "Content-Type": "application/json" }
 
 # @app.route("/post/register")
 # def register_voter():
