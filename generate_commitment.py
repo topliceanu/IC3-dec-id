f22f552762b16aefc5b0889b9817e5bdd1506a1f
Harjasleen Malvai <jasleenmalvai@gmail.com>
2023-06-17 19:57:06 -0400

    Signature running and checking

--- generate_commitment.py
+++ generate_commitment.py
@@ -2,9 +2,11 @@ from generate_mimc_hash import *
 import random
 
 
-def mimc_commit(msg):
-    r = random.getrandbits(128) % curve_order
-    print("r = ", r)
+def mimc_commit(msg, random):
+    r = 11
+    if random:
+        r = random.getrandbits(128) % curve_order
     com = MultiMiMC7(91, [msg, r], 11)
     return r, com 
+
 
--- generate_sig.py
+++ generate_sig.py
@@ -1,6 +1,7 @@
 import json
 import random
 from babyjubjub.ed25519 import *
+from generate_commitment import *
 from generate_mimc_hash import *
 # b = 255
 def publickey_l(sk):
@@ -23,7 +24,6 @@ def Hint_l(m):
         tmp.append(bit(h, i))
         tmp3 = str(bit(h, i)) + "+ " + str(out) + "+ " + str(out) + "=  "
         out = out + out + bit(h, i)
-        # print (tmp3 + str(out))
 
     # reverse endineess and return
 
@@ -35,34 +35,25 @@ def Hint_l(m):
 def generate_keys(use_random):
     sk = 123123
     if random:
-        sk = random.getrandbits(128) % l
-
+        sk = random.getrandbits(128) % curve_order
     pk = publickey_l(sk)
     return sk, pk
 
 
 def mimc_signature(m, sk, pk):
     h = str(MiMC7(91, sk, 0))
+    h_int = MiMC7(91, sk, 0)
     a = 2 ** (b - 2) + sum(2 ** i * bit(h, i) for i in range(3, b - 2))
-
-    # r = Hint_l(int(''.join([h[i] for i in range(int(b // 8), int(b // 4))])) + m)
-    r = 5
+    r = Hint_l(int(''.join([h[i] for i in range(int(b // 8), int(b // 4))])) + m) % l
     R = scalarmult(B, 8 * r)
-    h = MultiMiMC7(91, [R[0], R[1], pk[0], pk[1], m], 0) % l
-    print("Ah = ", scalarmult(pk,  h%l))
+    h = MultiMiMC7(91, [R[0], R[1], pk[0], pk[1], m], 0) % curve_order
     S = (r + (h * a)) % l
     return R, S
 
 
 def verify_sig(R, S, m, pk):
     A = pk
-    h = MultiMiMC7(91, [R[0], R[1], pk[0], pk[1], m], 0) % l
-    print("A = ", A)
-    print("8B = ", scalarmult(B, 8))
-    print("S8B = ", scalarmult(B, 8 * S))
-    print("h = ", h)
-    print("R = ", R)
-    print("Other side = ", pointAddition(R, scalarmult(A, (8 * h) % l)))
+    h = MultiMiMC7(91, [R[0], R[1], pk[0], pk[1], m], 0)
     if scalarmult(B, 8 * S) != pointAddition(R, scalarmult(A, (8 * h) % l)):
         raise Exception("signature does not pass verification")
 
@@ -82,16 +73,19 @@ def sign_to_json(msg, sk, pk):
         json.dump(data, f)
     return R, S
 
+def sign_to_data(msg, sk, pk):
+    R, S  = mimc_signature(msg, sk, pk)
+    data = {
+            'enabled': "1",
+            'Ax': str(pk[0]),
+            'Ay': str(pk[1]),
+            'S' : str(S),
+            'R8x': str(R[0]),
+            'R8y': str(R[1]),
+            'M': str(msg)}
 
 if __name__ == "__main__":
     sk, pk = generate_keys(False)
     msg = 16204137089086222846243685777293343290570733397750586346311362351531831223551
     R, S = sign_to_json(msg, 123123, pk)
     print("Verified: ", verify_sig(R, S, msg, pk))
-
-# sk, pk = generate_keys(False)
-# msg = 16204137089086222846243685777293343290570733397750586346311362351531831223551
-# R, S = sign_to_json(msg, 123123, pk)
-# print("Verified: ", verify_sig(R, S, msg, pk))
-#u = MultiMiMC7(2, [1, 1], 0)
-#print("\n Mimc = ", u, "\n")
