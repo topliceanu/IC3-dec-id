82093588c7054ce7e9e9d9f3939efb0bab68958c
roibarzur <roi.barzur@gmail.com>
2023-06-18 20:28:37 +0300

    Changed deployment argument

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ from eth_account import Account
 from compile import compile_contract
 
 
-def deploy_voting_contract(num_of_blocks: int = 100) -> str:
+def deploy_voting_contract(num_of_blocks: int = 10000) -> str:
     # Setup infura project ID and endpoint
     infura_url = "https://goerli.infura.io/v3/a342124bc854400c8e812ca793ebc06c"
 
